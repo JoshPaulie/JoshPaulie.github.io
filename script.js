c04b99fa2ca85fd367f9ec7b5ba62927f7e3e9e5
JoshPaulie (Bexli) <30572280+JoshPaulie@users.noreply.github.com>
2023-04-16 14:06:31 -0500

    🤦 Remove redundant 'making tutorials' hobby

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 const hobbyText = document.getElementById("hobby-text");
-const hobbies = ["python", "computer science", "neovim", "making tutorials"];
+const hobbies = ["python", "computer science", "neovim"];
 const random_hobby = hobbies[Math.floor(Math.random()*hobbies.length)];
 
 hobbyText.textContent = random_hobby;
