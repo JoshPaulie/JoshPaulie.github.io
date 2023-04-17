1d18fff6bde4e10a8f28418cae53f95fc18985cf
JoshPaulie (Bexli) <30572280+JoshPaulie@users.noreply.github.com>
2023-04-17 15:09:46 -0500

    ✨ Add some more hobbies

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 const hobbyText = document.getElementById("hobby-text");
-const hobbies = ["python", "computer science", "neovim"];
+const hobbies = ["python", "computer science", "neovim", "linux", "programming", "graphic design"];
 const random_hobby = hobbies[Math.floor(Math.random()*hobbies.length)];
 
 hobbyText.textContent = random_hobby;
