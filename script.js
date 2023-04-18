69e7129082fedcf4a14dfde78683cec83c4d917d
joshpaulie <joshpaullee@gmail.com>
2023-04-18 18:19:48 -0500

    ✨ Start the pizzazz

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 "use strict";
-const hobbyText = document.getElementById("hobby-text");
-const hobbies = ["python", "computer science", "neovim", "linux", "programming", "graphic design"];
-const random_hobby = hobbies[Math.floor(Math.random()*hobbies.length)];
+// Broken and don't have time to make branch
+function changeHobbyText() {
+    const hobbyText = document.getElementById("hobby-text");
+    const hobbies = ["python", "computer science", "neovim", "linux", "programming", "graphic design"];
+    const random_hobby = hobbies[Math.floor(Math.random()*hobbies.length)];
+    hobbyText.textContent = random_hobby;
+};
 
-hobbyText.textContent = random_hobby;
+changeHobbyText()
+
+hobbyText.addEventListener("click", () => {
+    changeHobbyText();
+});
