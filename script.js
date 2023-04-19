c8b878b3971599606bd2705d3553e195c0e8c92a
JoshPaulie (Bexli) <30572280+JoshPaulie@users.noreply.github.com>
2023-04-19 12:53:45 -0500

    🐞🥾 Fixed click on hobby "effect"

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 "use strict";
-// Broken and don't have time to make branch
+const hobbyText = document.getElementById("hobby-text");
 function changeHobbyText() {
-    const hobbyText = document.getElementById("hobby-text");
     const hobbies = ["python", "computer science", "neovim", "linux", "programming", "graphic design"];
     const random_hobby = hobbies[Math.floor(Math.random()*hobbies.length)];
     hobbyText.textContent = random_hobby;
 };
 
+// Run on "init"
 changeHobbyText()
 
+// todo
+// make it so that the random hobby isn't what is currently selected
 hobbyText.addEventListener("click", () => {
     changeHobbyText();
 });
