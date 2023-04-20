7f532f7a401c871f6d6db03fd211a3544d796591
JoshPaulie (Bexli) <30572280+JoshPaulie@users.noreply.github.com>
2023-04-19 20:30:24 -0500

    💅 Format w/ prettier

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 "use strict";
 const hobbyText = document.getElementById("hobby-text");
+
 function changeHobbyText() {
-    const hobbies = ["python", "computer science", "neovim", "linux", "programming", "graphic design"];
-    const random_hobby = hobbies[Math.floor(Math.random()*hobbies.length)];
-    hobbyText.textContent = random_hobby;
-};
+  const hobbies = ["python", "computer science", "neovim", "linux", "programming", "graphic design"];
+  const random_hobby = hobbies[Math.floor(Math.random() * hobbies.length)];
+  hobbyText.textContent = random_hobby;
+}
 
 // Run on "init"
-changeHobbyText()
+changeHobbyText();
 
 // todo
 // make it so that the random hobby isn't what is currently selected
 hobbyText.addEventListener("click", () => {
-    changeHobbyText();
+  changeHobbyText();
 });
